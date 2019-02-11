033f02014694fc2d89c12d7343b11d1f8103c82a
micare <michael.arenander@gmail.com>
2019-02-11 16:55:53 +0100

    Initialized basic code for the Asteroid Game

---
+++ XY_plane.c
@@ -0,0 +1,30 @@
+#include <stdint.h>     /* Declarations of uint_32 and the like */
+
+/*  XY_Plane______________________________________________________________________
+    This code may serve as a manager for any coordinate-axis related calculaltions
+    such as collisions between objects/sprites, movement and conversion to
+    Graphical Display data to be used and sent to the microship.
+*/
+
+
+// The planeToDisplay function will convert X,Y coordinates to the display's system
+void planeToDisplay (int x, int y)
+{
+    //we don't know what to return yet
+}
+
+
+
+// The plane function will use X,Y coordinate system to structure the game on.
+// TODO: Add an acceleration system? (maybe not needed)
+void plane ()
+{
+/*  Visualization of the input gridd
+
+    X---------->
+    11 12 13 14 Y
+    21 22 23 24 |
+    31 32 33 34 |
+    41 42 43 44 V
+*/
+}

e6d331c30467192ccbc04d31381be8289a4ae0ca
micare <michael.arenander@gmail.com>
2019-02-12 14:17:11 +0100

    consoleVersion structured

--- consoleLaunch.c
+++ consoleLaunch.c
@@ -1,7 +1,62 @@
 #include <stdio.h> //importing standard io
 
-int main (void)
+void consoleRender (int x, int y)
 {
-    printf("managed to launch game\n");
+    int width = 128;
+    int height = 32;
+    int display[width][height];
+    for (int i = 0; i < height; i++ )
+    {
+        for (int j = 0; j < width; j++ )
+        {
+            if(y == i && x == j)
+            {
+                printf("X");
+            }
+            else
+            {
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
 }
 
+
+/*void draw (int art[][])
+{
+    printf("sizeof(art) = %d \n", sizeof(art));
+    //for (int i = 0; i < sizeof(art))
+*/
+
+
+int main (void)
+{
+    printf("managed to launch game:\n");
+
+    /*int ret[][] =
+    {
+        {0,0,0,1,0,0,0},
+        {0,0,1,1,1,0,0},
+        {0,0,1,0,1,0,0},
+        {0,1,0,1,0,1,0},
+        {1,0,1,0,1,0,1},
+        {1,0,1,0,1,0,1},
+        {1,0,0,1,0,0,1}
+    };*/
+
+    consoleRender(0,1);
+
+    //drawing:
+    /*
+    ---x---
+    --xxx--
+    --x-x--
+    -x-x-x-
+    x-x-x-x
+    x-x-x-x
+    x--x--x
+*/
+
+    //image file:
+}

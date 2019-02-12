de665b3ed80345539733c996278ca9312d8b8d92
Joakim <joakim.tornert@outlook.com>
2019-02-12 12:06:22 +0100

    First working compilation

--- spaceship.c
+++ spaceship.c
@@ -1,6 +1,5 @@
 #include <stdint.h>     /* Declarations of uint_32 and the like */
 #include <pic32mx.h>    /* Declarations of system-specific addresses etc */
-#include XY_plane.c     /* Document to use for any graphical relocation or calculations */
 
 // The spaceship function works as the spaceship's main function that manages spaceship-specific activities.
 void spaceship ()
@@ -8,6 +7,6 @@ void spaceship ()
     int health = 1; // Health to use for the game, at 1 for initial testing purpose.
     int x = 20;     // X-value to input to the coordinate system. Spawning at initial value.
     int y = 20;     // Y-value to input to the coordinate system. Spawning at initial value.
-    
-    
+
+
 }
